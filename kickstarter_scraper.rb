90bb45cec20695f270ef243926ed4d3636697752
Natalie J <natsetc@gmail.com>
2019-06-08 00:59:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# location: project.css(".location-name").text (line 790)
+# location: project.css(".location-name").text (line 790) (both line 8 and 9 are correct)
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
